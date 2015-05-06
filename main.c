5336f672e3a223f4a7dc4f5ee17f4a2dbe377756
user <ajw9110@gmail.com>
2015-05-06 11:20:44 -0400

    jh

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "pbm.h"
+#include "img_proc.h"
+
+int main(int argc, const char * argv[]) {
+
+    char *data;
+    struct image_header ih;
+    unsigned long data_size;
+    
+    if (argc != 3) {
+        printf("Not enought arguments, the program call should as follows: %s <input.pbm> <output.pbm>", argv[0]);
+        return 0;
+    }
+    
+    data = readImage(argv[1], &data_size, &ih);
+    rgb2ycbcr(data, ih.cols, ih.rows);
+    ycbcr2rgb(data, ih.cols, ih.rows);
+
+    if(data != NULL)
+        writeImage(argv[2], data, ih);
+    
+    return 0;
+}
